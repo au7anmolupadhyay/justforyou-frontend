7dfcae99cc945615781175fe0e58d405f3b2bcda
au7anmolupadhyay <anmolupadhau7@gmail.com>
2026-01-08 15:47:24 +0530

    added authStore logic

--- src/store/authStore.ts
+++ src/store/authStore.ts
@@ -2,12 +2,27 @@ import { create } from "zustand";
 
 type AuthState = {
   isLoggedIn: boolean;
+  initAuth: () => void;
   login: () => void;
   logout: () => void;
 };
 
 export const useAuthStore = create<AuthState>((set) => ({
   isLoggedIn: false,
-  login: () => set({ isLoggedIn: true }),
-  logout: () => set({ isLoggedIn: false }),
+
+  // called once on app start
+  initAuth: () => {
+    const token = localStorage.getItem("accessToken");
+    set({ isLoggedIn: !!token });
+  },
+
+  login: () => {
+    set({ isLoggedIn: true });
+  },
+
+  logout: () => {
+    localStorage.removeItem("accessToken");
+    localStorage.removeItem("refreshToken");
+    set({ isLoggedIn: false });
+  },
 }));
